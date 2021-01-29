53aea999b286ca4653b189e1c6be045b223b559e
jeromeshan <43412578+jeromeshan@users.noreply.github.com>
2021-01-30 02:18:12 +0300

    tetris mechanic

---
+++ Assets/SpawnBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlock : MonoBehaviour
+{
+
+    public GameObject[] Tetrominoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetraminoes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewTetraminoes()
+    {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
--- Assets/TetrisBlock.cs
+++ Assets/TetrisBlock.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,17 @@ public class TetrisBlock : MonoBehaviour
 {
     public Vector3 rotationPoint;
     private float previousTime;
-    public static float fallTime = 4.8f;
+    public static float fallTime = 0.8f;
     public static int height =20;
     public static int width = 10;
+
+    public GameObject gameOverText;
+    private static Transform[,] grid = new Transform[width, height];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -34,7 +38,10 @@ public class TetrisBlock : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90); ;
+
+
+
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90); 
             if (!isValidMove())
                 transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90); ;
         }
@@ -44,8 +51,43 @@ public class TetrisBlock : MonoBehaviour
         {
             transform.position += new Vector3(0, -1, 0);
             if (!isValidMove())
+            {
                 transform.position += new Vector3(0, 1, 0);
+
+                AddToGrid();
+                CheckLines();
+                this.enabled = false;
+                if (!isGameOver())
+                    FindObjectOfType<SpawnBlock>().NewTetraminoes();
+                else
+                {
+                    FindObjectOfType<UnityEngine.UI.Text>().text = "Game Over" ;
+                }
+            }
             previousTime = Time.time;
+        }
+
+        bool isGameOver()
+        {
+            foreach (Transform child in transform)
+            {
+                int roundedX = Mathf.RoundToInt(child.transform.position.x);
+                int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+                if (roundedY > 17)
+                    return true;
+            }
+            return false;
+        }
+        void AddToGrid()
+        {
+            foreach (Transform child in transform)
+            {
+                int roundedX = Mathf.RoundToInt(child.transform.position.x);
+                int roundedY = Mathf.RoundToInt(child.transform.position.y);
+
+                grid[roundedX, roundedY] = child;
+            }
         }
 
         bool isValidMove()
@@ -54,11 +96,63 @@ public class TetrisBlock : MonoBehaviour
             {
                 int roundedX = Mathf.RoundToInt(child.transform.position.x);
                 int roundedY = Mathf.RoundToInt(child.transform.position.y);
+                if (roundedX < 0 || roundedX >= width || roundedY <0 || roundedY >= height)
+                    return false;
 
-                if (roundedX <= 0 || roundedX > width || roundedY <=0 || roundedY > height)
+                if (grid[roundedX, roundedY] != null)
                     return false;
             }
             return true;
         }
+    }
+
+    private void CheckLines()
+    {
+        for (int i = height-1; i >=0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    private void RowDown(int i)
+    {
+        for (int k = i; k < height; k++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j,k] != null)
+                {
+                    grid[j, k - 1] = grid[j, k];
+                    grid[j, k] = null;
+                    grid[j, k - 1].transform.position += new Vector3(0, -1, 0);
+                }
+
+            }
+        }
+
+    }
+
+    private void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[ j,i].gameObject);
+            grid[j,i] = null;
+                
+        }
+    }
+
+    private bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[ j,i] == null)
+                return false;
+        }
+        return true;
     }
 }
