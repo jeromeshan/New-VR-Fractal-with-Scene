e05710acfc2d669739cf2de91a8c9b18f5d94791
nchuykin <nkchuykin@edu.hse.ru>
2021-04-03 21:23:07 +0300

    new skyboxes, katana add, slice in progress

--- Assets/BladeShow.cs
+++ Assets/BladeShow.cs
@@ -11,7 +11,6 @@ public class BladeShow : MonoBehaviour
     public GameObject leftHand;
     public GameObject blade;
 
-
     // Start is called before the first frame update
     void Start()
     {
